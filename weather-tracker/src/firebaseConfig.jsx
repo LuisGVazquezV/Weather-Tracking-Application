322c88daf1be299fab64ac387e5f4e6c6635a963
LuisGVazquezV <169108816+LuisGVazquezV@users.noreply.github.com>
2025-03-12 15:42:34 +0000

    Added files for project

---
+++ weather-tracker/src/firebaseConfig.jsx
@@ -0,0 +1,9 @@
+// src/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app
