2984691133bcc5dd43815da5c6cec7d44eb9a272
LuisGVazquezV <169108816+LuisGVazquezV@users.noreply.github.com>
2025-03-14 18:51:27 +0000

    removed personal info

--- weather-tracker/src/firebaseConfig.jsx
+++ weather-tracker/src/firebaseConfig.jsx
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCK102JFR29-wlGxn1Tv0YwkuWt9btmugk",
-  authDomain: "react-project-6377c.firebaseapp.com",
-  projectId: "react-project-6377c",
-  storageBucket: "react-project-6377c.firebasestorage.app",
-  messagingSenderId: "20230706422",
-  appId: "1:20230706422:web:58a00d339c89183a0208d7"
+
 };
 
 // Initialize Firebase
